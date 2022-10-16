007e59809b3aee516660fa1105746107c577e5db
Stanislav Shevchenko <StasShev87@gmail.com>
2022-10-16 20:53:59 +0300

    Add task 02 gallery markup

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,16 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryItemsMarkup = galleryItems
+  .map((item) => {
+    return `
+<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`;
+  })
+  .join("");
+
+const gallery = document.querySelector("ul.gallery");
+gallery.insertAdjacentHTML("beforeend", galleryItemsMarkup);
