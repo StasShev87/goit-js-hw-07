d5aec725c70335a00f4a0d2b9cd89e517e8a5974
Stanislav Shevchenko <StasShev87@gmail.com>
2022-10-16 19:48:11 +0300

    Add gallery markup, prevent <a> link redirect, obtain large image url

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+// Creating and rendering markup from the galleryItems data array and provided gallery item template.
+const galleryItemsMarkup = galleryItems
+  .map((item) => {
+    return `
+<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`;
+  })
+  .join("");
+
+const gallery = document.querySelector("div.gallery");
+gallery.insertAdjacentHTML("beforeend", galleryItemsMarkup);
+
+// Implementing delegation to div.gallery and getting the url of a large image.
+gallery.addEventListener("click", (event) => {
+  // Prevent open <a> link action
+  event.preventDefault();
+
+  // Get a large image url
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const largeImageUrl = event.target.dataset.source;
+
+  console.log(largeImageUrl);
+});
+
+// Adding the script and styles of the modal window library basicLightbox. Use the jsdelivrCDN service and add links to minified (.min) library files to your project.
+// Opening a modal window by clicking on a gallery item. To do this, check out the documentation and examples.
+// Replacing the value of the src attribute of the <img> element in a modal window before opening. Use the ready-made modal window markup with the image from the examples of the basicLightbox library.
